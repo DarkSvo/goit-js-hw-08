aac5b669bd7a3b5110f33fa2e45ed95849037bbb
Anton Hodorlov <95712062+DarkSvo@users.noreply.github.com>
2023-05-25 19:37:18 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,27 +1,26 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryList = document.querySelector(`.gallery`);
 
-const gallery = document.querySelector(".gallery");
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => { 
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+  </a>
+  </li>`;
+  })
+    .join("");
+  
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
 
-const items = galleryItems.map(({ preview, original, description }) => `
-<div>
-<a class="gallery__item" href="${original}">
-    <img class="gallery__image"
-    src="${preview}" 
-    alt="${description}" />
-</a>
-</div>
-`).join("");
-    
-    gallery.insertAdjacentHTML("afterbegin", items);
-
-new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
 });
+
 
 console.log(galleryItems);

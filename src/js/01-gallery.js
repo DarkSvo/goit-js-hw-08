57e2815bcb015717263c57a04cf3c14acadb525f
Anton Hodorlov <95712062+DarkSvo@users.noreply.github.com>
2023-05-26 22:03:00 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,26 +1,32 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-const galleryList = document.querySelector(`.gallery`);
+const galleryRef = document.querySelector('.gallery');
 
-const galleryMarkup = galleryItems.map(({ preview, original, description }) => { 
-  return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-  <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
-  </a>
-  </li>`;
-  })
-    .join("");
-  
-galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+const renderGallery = imgs =>
+  imgs.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+   </a>
+</li>`,
+    ''
+  );
 
-const lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
+const insertRenderGallery = string => {
+  galleryRef.insertAdjacentHTML('beforeend', string);
+};
+
+insertRenderGallery(renderGallery(galleryItems));
+
+let gallery = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
+  captionsData: 'alt',
 });
-
+gallery.on('show.simplelightbox', function () {});
 
 console.log(galleryItems);
